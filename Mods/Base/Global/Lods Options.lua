40e90f65f978f35e7bf1cf75de00e6b15b47e9b9
Sergey "GrayFace" Rozhenko <sergroj@mail.ru>
2013-12-13 16:43:44 +0700

    Moving trees option

--- Mods/Base/Global/Lods Options.lua
+++ Mods/Base/Global/Lods Options.lua
@@ -4,8 +4,10 @@ function global.events.EnterContext()
 	if context ~= "map" then
 		return
 	end
-	if Options.BrighterTrees then
+	if Options.Trees == "bright" then
 		LD:T(?v, 11, ModName.."/Lods/Trees.lod")
+	elseif Options.Trees == "move" then
+		LD:T(?v, 11, ModName.."/Lods/TreeMove.lod")
 	end
 	if Options.NewGrid then
 		LD:T(?v, 11, ModName.."/Lods/Grid.lod")
