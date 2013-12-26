66a44ea9956bd7aad1f675a43a618725c0593fc4
Sergey "GrayFace" Rozhenko <sergroj@mail.ru>
2013-12-26 18:16:39 +0700

    [-] SoD bug: sound quality was low (22kHz, mono). (Alpha 6.1)

--- T1/global_hooks.h
+++ T1/global_hooks.h
@@ -455,6 +455,8 @@ __newCallers newCallers[]=
 	{0x40770E, (long)_FixLoadActiveHero, H_CALL5}, // Don't reset current hero when loading a game
 	{0x600530, 0x600532, H_NOP}, // Strange hanging bug
 	{0x4C5D09, 0x4C5D0F, H_NOP}, // Don't limit events text length to 65535 characters
+	{0x684AF8, 0xAC44, H_DWORD}, // Use 44khz
+	{0x684B00, 2, H_DWORD}, // Use stereo
 
 	{0}
 };
